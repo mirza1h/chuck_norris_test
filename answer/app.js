fadf6c53b6ff5752447ff7f5ba7cc3bdf86670fc
Mirza Hasanovic <mirza.hasanovic1@fet.ba>
2022-01-16 11:20:14 +0100

    perk2 improved

--- answer/app.js
+++ answer/app.js
@@ -7,13 +7,16 @@ document.addEventListener("DOMContentLoaded", () => {
   // Registers event handlers
   // Set max number for input and loads initial jokes
   function init() {
-    document.getElementById("idPlayBtn").addEventListener("click", () => onBtnClicked());
-    Array.from(jokeDivs).forEach(div => div.addEventListener("dragend", (event) => onDragEnd(event)));
-
-    fetchJokesCount().then((count) => {
-      jokeInput.max = count - 3;
-    });
+    registerEventHandlers(onBtnClicked, jokeDivs, onDragEnd);
+    fetchJokesCount().then((count) => { jokeInput.max = count - 3; });
     loadRandomJokes();
+  }
+
+  // Adds 'click' and 'dragend' event handlers
+  function registerEventHandlers(onBtnClicked, jokeDivs, onDragEnd) {
+    document.getElementById("idPlayBtn").addEventListener("click", () => onBtnClicked());
+    const allDivs = document.getElementsByClassName('tile');
+    Array.from(allDivs).forEach(div => div.addEventListener("dragend", (event) => onDragEnd(event)));
   }
 
   // Event handler for button 'click' event
@@ -43,6 +46,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // Keeps fetching jokes until 3 unique jokes are loaded and then sets them to divs
+  // NOTE: API provides a way to get any number of unique random jokes (http://api.icndb.com/jokes/random/<number>)
+  // but task description seems to require use of https://api.icndb.com/jokes/random/
   function loadRandomJokes() {
     var setOfJokes = new Set();
     var keepFetching = function () {
@@ -68,50 +73,44 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Fetches one random joke object from API
   function fetchRandomJoke() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       fetchFromAPI('https://api.icndb.com/jokes/random/').then(value => resolve(value.joke));
     });
   }
 
   // Fetches one joke object by ID from API
   function fetchJokeById(id) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       fetchFromAPI(`https://api.icndb.com/jokes/${id}`).then(value => resolve(value.joke));
     });
   }
 
   // Fetches total number of jokes
   function fetchJokesCount() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       fetchFromAPI('https://api.icndb.com/jokes/count').then(value => resolve(value));
     });
   }
 
-  // Fetches data from priveded URL and returns parsed object
+  // Fetches data from provided URL and returns parsed object
   function fetchFromAPI(url) {
     return new Promise((resolve, reject) => {
       fetch(url).then(response => response.json()).then((data) => {
         resolve(data.value);
       }).catch((err) => {
-        reject(err);
+        alert(err.message);
+        reject();
       });
     });
   }
 
   // Event handler for button 'dragend' event
-  // Swap content and classes of selected tile with target tile
+  // Swaps selected and target tile
   function onDragEnd(event) {
-    const selectedTile = event.target;
-    var targetTile = getTargetTile(event);
-    var tempTile = selectedTile.cloneNode(true);
-    targetTile.classList.replace('right', 'left')
-    targetTile.classList.replace('left', 'right')
-    selectedTile.innerHTML = targetTile.innerHTML;
-    selectedTile.classList = targetTile.classList;
-    targetTile.innerHTML = tempTile.innerHTML;
-    targetTile.classList = tempTile.classList;
+    swapTiles(event.target, getTargetTile(event));
   }
 
+  // Get the tile element positioned at the coordinates where 'dragend' event finished
   function getTargetTile(event) {
     var element = document.elementFromPoint(event.clientX, event.clientY);
     if (element && element.draggable)
@@ -119,6 +118,20 @@ document.addEventListener("DOMContentLoaded", () => {
     else {
       return event.target;
     }
+  }
+
+  // Swap content and classes of selected tile with target tile
+  function swapTiles(selectedTile, targetTile) {
+    selectedTile.classList.replace('right', 'left');
+    selectedTile.classList.replace('left', 'right');
+    var tempTile = selectedTile.cloneNode(true);
+    targetTile.classList.replace('right', 'left');
+    targetTile.classList.replace('left', 'right');
+    var tempTargetTile = targetTile.cloneNode(true);
+    targetTile.innerHTML = tempTile.innerHTML;
+    targetTile.classList = tempTile.classList;
+    selectedTile.innerHTML = tempTargetTile.innerHTML;
+    selectedTile.classList = tempTargetTile.classList;
   }
 
   init();
